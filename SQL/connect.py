cf92b5abfc1a883b00800576225da07a70ce7a3a
frederic.gillardo <frederic.gillardo@cta-consortium.org>
2024-03-27 15:28:04 +0100

    update connect.py

--- SQL/connect.py
+++ SQL/connect.py
@@ -1,6 +1,30 @@
 import psycopg2
 from config import load_config
 
+
+def create_tables(config):
+    """ Create tables in the PostgreSQL database"""
+    commands = [
+            """
+            CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy');
+            """,
+            """
+            CREATE TABLE activity_metadata5 (
+                activity_metadata_id SERIAL PRIMARY KEY,
+                activity_metadata_name VARCHAR(255) NOT NULL
+            )"""
+        ]  
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                for command in commands:
+                    print(command)
+                    cur.execute(command)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
 def connect(config):
     """ Connect to the PostgreSQL database server """
     try:
@@ -14,4 +38,4 @@ def connect(config):
 
 if __name__ == '__main__':
     config = load_config()
-    connect(config)
+    create_tables(config)
