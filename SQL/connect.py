d0ae87b1bb114e74b2ba48abb0bb528c41990175
gillardoLapp <37876051+gillardoLapp@users.noreply.github.com>
2025-02-24 10:38:59 +0100

    Update connect.py

--- SQL/connect.py
+++ SQL/connect.py
@@ -11,7 +11,28 @@ def connect(config):
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
+def display_table(config):
+    commands = [
+        """
+        SELECT table_name FROM information_schema.tables
+        WHERE table_schema = 'public'
+        """
+        ]
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                for command in commands:
+                    print(command)
+                    cur.execute(command)
+                    for table in cur.fetchall():
+                        print(table)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
 
 if __name__ == '__main__':
     config = load_config()
     connect(config)
+    display_table(config)
